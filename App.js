05a50ede8a073aa79102d6cd38de852b5caeeae0
Alice-in-woolieland <119767457+Alice-in-woolieland@users.noreply.github.com>
2024-02-12 14:30:26 -0800

    firstcommit

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, useState} from 'react-native';
+import PetView from './vPetStates/happy';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hatsune miku</Text>
-      <image style={styles.logo} source={require('./vPetimages/mikusway.gif')}/>
-      <StatusBar style="auto" />
-    </View>
+	
+    <SafeAreaView style={styles.container}>
+	
+      <Text style={styles.paragraph}>
+	
+        The United{"\n"}States{"\n"}
+	
+        of  MIKU
+	
+      </Text>
+	
+      <Card>
+	
+        <PetView pet={{mood, text, image, buttonDisabled}} handleOnPress={() => handleButtonPress()}/>
+	
+      </Card>
+	
+      
+	
+    </SafeAreaView>
+	
   );
 }
 
@@ -18,8 +36,127 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonrow: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    width:200,
+    fontSize:1,
+  },
+  title: {
+    fontSize:50,
+    fontWeight:'bold',
+  },
   logo: {
-    height: 128,
-    width: 128,
+    margin:50,
   }
 });
+
+const Mood = {
+  Lonely: 'Lonely',
+  Happy: 'Happy',
+  Dancing: 'Dancing',
+  Sleepy: 'Sleepy'
+}
+
+const happyText = "Happy";
+const dancingText = "Dancing!";
+const sleepyText = "Sleepy...";
+const lonelyText = "Lonely";
+
+const happyImage = require("./vPetimages/mikusway.gif");
+const dancingImage = require("./vPetimages/hatsune-miku.gif");
+const sleepyImage = require("./vPetimages/mikuamp.gif");
+const lonelyImage = require("./vPetimages/hatsunepost.gif");
+
+const [mood, setMood] = useState(Mood.Happy);
+const [text, setText] = useState(happyText);
+const [image, setImage] = useState(happyImage);
+const [buttonDisabled, setButtonDisabled] = useState(true);
+
+
+function moodTransition (newMood) {
+
+  switch (newMood) {
+
+    case Mood.Dancing:
+
+      setMood(Mood.Dancing)
+
+      setText(dancingText)
+
+      setImage(dancingImage);
+
+      setButtonDisabled(false);
+
+      break;
+
+    case Mood.Lonely:
+
+      setMood(Mood.Lonely)
+
+      setText(lonelyText)
+
+      setImage(lonelyImage);
+
+      setButtonDisabled(true);
+
+      break;
+
+    case Mood.Sleepy:
+
+      setMood(Mood.Sleepy)
+
+      setText(sleepyText)
+
+      setImage(sleepyImage);
+
+      setButtonDisabled(false);
+
+      break;
+
+    case Mood.Happy:
+
+      setMood(Mood.Happy)
+
+      setText(happyText)
+
+      setImage(happyImage);
+
+      setButtonDisabled(true);
+
+      break;
+
+  }
+
+}
+const handleButtonPress = () => {
+  switch (mood) {
+    case Mood.Lonely:
+      moodTransition(Mood.Happy)
+      break;
+    case Mood.Dancing:
+      moodTransition(Mood.Sleepy)
+      break;
+  }
+};
+useEffect(() => {
+  const timerId = setInterval(() => {
+    switch (mood) {
+      case Mood.Happy:
+        if ((Math.random() * 10) < 1) {
+          moodTransition(Mood.Dancing);
+        }
+        break;
+      case Mood.Sleepy:
+        if ((Math.random() * 10) < 2) {
+          moodTransition(Mood.Lonely)
+        }
+        break;
+    }
+
+  }, 1000);
+
+  return () => clearInterval(timerId);
+});
+
+
---
+++ vPetStates/happy.js
@@ -0,0 +1,73 @@
+
+import { Button, StyleSheet, Text, View, Image, useState} from 'react-native';
+
+const PetView = ({ pet, handleOnPress }) => {
+    let moodColor = styles.logoHappy;
+  
+    switch (pet.mood) {
+      case "angry":
+        moodColor = styles.logoAngry;
+        break;
+      case "curious":
+        moodColor = styles.logoCurious;
+        break;
+      
+      case "happy":
+        moodColor = styles.logoHappy;
+        break;
+      
+      case "lonely":
+        moodColor = styles.logoLonely;
+        break;
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+         {pet.text}
+        </Text>
+        <Image style={moodColor} source={pet.image} />
+        <Button disabled={pet.buttonDisabled} title="Play with!" onPress={handleOnPress}></Button>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 24,
+    },
+    paragraph: {
+      margin: 24,
+      marginTop: 0,
+      fontSize: 14,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    logoAngry: {
+      height: 128,
+      width: 128,
+      borderColor: "red",
+      borderWidth: 3,
+    },
+    logoCurious: {
+      height: 128,
+      width: 128,
+      borderColor: "purple",
+      borderWidth: 3,
+    },
+    logoLonely: {
+      height: 128,
+      width: 128,
+      borderColor: "blue",
+      borderWidth: 3,
+    },
+    logoHappy: {
+      height: 128,
+      width: 128,
+      borderColor: "green",
+      borderWidth: 5,
+    },
+  });
+  
+  export default PetView;
